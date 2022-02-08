d919780715906d69c7c73fff3c6001d442ddbd9d
ArindamSharma <arindamsharma1998@gmail.com>
2022-02-08 14:53:38 +0530

    added favicon

---
+++ main.js
@@ -0,0 +1,6 @@
+async function OnLoadFunction(){
+    console.log("Redirection to Profile V1")
+    // await new Promise(resolve => setTimeout(resolve, 1000));
+    // window.location.replace("https://arindamsharma.github.io/profilev1");
+}
+window.onpaint=OnLoadFunction();
