e13efe38965c9b7a1b3a1838ca85f861b096efde
ArindamSharma <arindamsharma1998@gmail.com>
2025-06-24 11:43:53 +0530

    fixed project cards

--- react/src/components/section/project.js
+++ react/src/components/section/project.js
@@ -7,23 +7,38 @@ import ProjectCard from "../ProjectCard";
 function ProjectSection() {
 	return (
 		<section className="projects-section" id="projects">
-			<h2>Work So Far</h2>
-			<div className="projects-section-grid">
-				{PROJECTS.slice(0, 4).map((project, idx) => (
+			<div className="projects-section-col1">
+				<h2 className="projects-section-header">Work So Far</h2>
+				<div className="projects-section-grid">
+					{PROJECTS.slice(0, 2).map((project, idx) => (
+						<ProjectCard
+							key={idx}
+							project={project}
+							navigateTo={`/project/${project.id !== undefined ? project.id : idx}`}
+							showLink={true}
+						/>
+					))}
+				</div>
+			</div>
+			<div className="projects-section-col2">
+				<div className="projects-section-grid">
+					{PROJECTS.slice(2,4).map((project, idx) => (
+						<ProjectCard
+							key={idx + 4}
+							project={project}
+							navigateTo={`/project/${project.id !== undefined ? project.id : idx + 4}`}
+							showLink={true}
+						/>
+					))}
+
+					{/* See More Project Card */}
 					<ProjectCard
-						key={idx}
-						project={project}
-						navigateTo={`/project/${project.id !== undefined ? project.id : idx}`}
-						showLink={true}
+						key={PROJECTS.length}
+						project={{ title: "See More Projects →" }}
+						navigateTo="/projectslist"
+						showLink={false}
 					/>
-				))}
-				{/* See More Project Card */}
-				<ProjectCard
-					key={PROJECTS.length}
-					project={{ title: "See More Projects →" }}
-					navigateTo="/projectslist"
-					showLink={false}
-				/>
+				</div>
 			</div>
 		</section>
 	);
