c02e03fffac39ba70d8ce405fb1f0421fc32e934
ArindamSharma <arindamsharma1998@gmail.com>
2025-07-02 13:10:19 +0530

    checkpoint13

--- react/src/components/section/about.js
+++ react/src/components/section/about.js
@@ -32,7 +32,7 @@ function AboutSection() {
                             >
                                 Let's Work Together
                             </button>
-                            <button className="btn-secondary cv-btn">
+                            <button className="btn-secondary">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" width="16" height="16" style={{marginRight: '8px'}}>
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 004.561 21h14.878a2 2 0 001.94-1.515L22 17"/>
                                 </svg>
