3faafc6b72efe2ac816efc5aa630f4b411eb415e
ArindamSharma <arindamsharma1998@gmail.com>
2025-07-02 22:29:48 +0530

    build2

--- src/old/main.js
+++
@@ -1,6 +0,0 @@
-async function OnLoadFunction(){
-    console.log("Redirection to Profile V1")
-    // await new Promise(resolve => setTimeout(resolve, 1000));
-    // window.location.replace("https://arindamsharma.github.io/profilev1");
-}
-window.onpaint=OnLoadFunction();
